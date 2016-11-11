a5d177500317256a68254b1015873a3378f43082
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-11 13:15:06 +0100

    redirect

--- r-travis/scripts/travis-tool.sh
+++ r-travis/scripts/travis-tool.sh
@@ -138,7 +138,7 @@ EnsureDevtools() {
         # Install devtools and testthat.
         RBinaryInstall testthat curl
         RSourceInstall remotes
-        Rscript -e "library(remotes);install_url('https://github.com/krlmlr/devtools/raw/develop/bin/devtools_1.12.0.9001.zip')"
+        Rscript -e "library(remotes);install_url('https://raw.githubusercontent.com/krlmlr/devtools/develop/bin/devtools_1.12.0.9001.zip')"
     fi
 }
 
