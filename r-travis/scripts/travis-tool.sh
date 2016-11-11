9936f1133ef5232cd4306ef84025dc30d87302ca
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-11 13:09:54 +0100

    also need curl

--- r-travis/scripts/travis-tool.sh
+++ r-travis/scripts/travis-tool.sh
@@ -136,7 +136,7 @@ BootstrapMacOptions() {
 EnsureDevtools() {
     if ! Rscript -e 'if (!("devtools" %in% rownames(installed.packages()))) q(status=1)' ; then
         # Install devtools and testthat.
-        RBinaryInstall testthat
+        RBinaryInstall testthat curl
         RSourceInstall remotes
         Rscript -e "library(remotes);install_url('https://github.com/krlmlr/devtools/raw/develop/bin/devtools_1.12.0.9001.zip')"
     fi
